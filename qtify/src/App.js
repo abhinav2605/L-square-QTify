3d3f83c6f810279751e612f67294d9cc864c12c8
abhinav kumar <abhinav2605@yahoo.com>
2024-04-09 00:40:39 +0530

    Test commit6

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,13 @@
-import Navbar from './components/navbar/navbar'
+import Navbar from './components/navbar/navbar';
+import Hero from './components/Hero Section/hero';
+
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
